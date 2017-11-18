2b7801e7bca79a768ee539758ced561079bacdaa
Sysboon Technologies <sysboontechnologies@Sysboons-MacBook-Air.local>
2017-11-19 01:23:53 +0530

    branch commit

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,14 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Sysboon Technologies on 11/19/17.
+//  Copyright © 2017 Sysboon Technologies. All rights reserved.
+//
+
+import Foundation
+
+
+class MapForItinerary: NSObject {
+    
+}
